63c5da041665f24cbc09bf4f6839771d49245ac5
oufresh <sergio.gennari@gmail.com>
2018-05-06 13:38:56 +0200

    animated

--- react-libs-test/src/App.js
+++ react-libs-test/src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { FlexTest } from './views/FlexTest';
 import { MClassElem } from './views/Multiple';
+import { Animated } from './views/Animated';
 import './App.css';
 
 class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.visible = false;
+  }
+  onClick = () => {
+    this.visible = true;
+  }
   render() {
     return (
       <div className="app">
         <h1 className="app-title">Welcome to React tests</h1>
         <FlexTest />
         <MClassElem className={"text-color"} />
+        <button onClick={this.onClick}>show</button>
+        <Animated visible={this.visible} />
       </div>
     );
   }
--- react-libs-test/src/views/Animated.js
+++ react-libs-test/src/views/Animated.js
@@ -1,3 +1,41 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 
-//const 
+const Anim = styled.div`
+    display: block;
+    background: pink;
+    width: 200px;
+    height: 200px;
+    transition: opacity 500ms ease-in-out;
+    opacity: 0;
+`;
+
+export class Animated extends React.Component {
+    constructor(props) {
+        super(props);
+        //this.state.visible = props.visible;
+    }
+
+    /*componentDidMount() {
+        if (this.props.visible === true) {
+            let node = ReactDOM.findDOMNode(this);
+            node.style.opacity = 0;
+        }
+    }*/
+
+    componentWillReceiveProps(nextProp) {
+        if (nextProp.visible === true) {
+            let node = ReactDOM.findDOMNode(this);
+            node.style.opacity = 1;
+        }
+    }
+
+    render() {
+      return (
+        <Anim>
+            <h1>Hello!!!</h1>
+        </Anim>
+      );
+    }
+};
