c7f790e6624947c5bef3bf68b12da4e5e6e18c46
oufresh <sergio.gennari@gmail.com>
2018-07-17 18:11:12 +0200

    router pre prove

--- redux-observable-test/src/App.js
+++ redux-observable-test/src/App.js
@@ -3,13 +3,38 @@ import { Provider } from 'react-redux';
 import './App.css';
 import store from './store/store';
 import Ping from './view/ping/Ping';
+import { Switch, Route, Link } from 'react-router-dom';
+
+const Home = () => <div />;
+
+const Header = () => (
+  <header>
+    <nav>
+      <ul style={{listStyle: 'none'}}>
+        <li><Link to='/' replace>Home</Link></li>
+        <li><Link to='/ping' replace>Ping</Link></li>
+      </ul>
+    </nav>
+  </header>
+);
+
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route path='/ping' component={Ping}/>
+    </Switch>
+  </main>
+);
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <div className="App">
-          <Ping />
+          <Header />
+          <hr />
+          <Main />
         </div>
       </Provider>
     );
