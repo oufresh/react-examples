ac01ffc5902c74fbcd096a47a1899c011797f5f2
Sergio Gennari <sergio.gennari@softeco.it>
2019-03-28 14:20:51 +0100

    test modules css

---
+++ some-hooks/src/global/CounterButton.module.css.d.ts
@@ -0,0 +1,2 @@
+declare const counterButton: string;
+export { counterButton };
--- some-hooks/src/global/StateFullHello.tsx
+++ some-hooks/src/global/StateFullHello.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { store, useStore } from './hookstore'
+import { CounterButton } from "./CounterButton";
 
 // setting the store initial state
 store.state = 0
@@ -10,12 +11,11 @@ function StatefulHello() {
   return (
     <div>
       <h2>The button inside this component was clicked {timesClicked} times</h2>
-      <button
-        type="button"
+      <CounterButton
         onClick={() => updateTimesClicked(timesClicked + 1)}
       >
         Update
-      </button>
+      </CounterButton>
     </div>
   )
 }
