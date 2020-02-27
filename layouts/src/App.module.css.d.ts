482835ca12cd8e3ac31ca76de54bedaf4f697945
Sergio Gennari <sergio.gennari@softeco.it>
2020-02-27 17:44:35 +0100

    tool

---
+++ layouts/src/App.module.css.d.ts
@@ -0,0 +1,13 @@
+declare const styles: {
+  readonly "App": string;
+  readonly "Header": string;
+  readonly "Main": string;
+  readonly "Toolbox": string;
+  readonly "ToolboxRow": string;
+  readonly "ToolBoxRowElem": string;
+  readonly "ControlBox": string;
+  readonly "tooltip": string;
+  readonly "tooltiptext": string;
+};
+export = styles;
+
