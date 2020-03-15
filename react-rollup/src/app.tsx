e6c55634f214e2bbe0989a17b0f3e216a6ec3091
Sergio Gennari <sergio.gennari@softeco.it>
2020-03-15 19:58:20 +0100

    try ref

--- react-rollup/src/app.tsx
+++ react-rollup/src/app.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import * as styles from "./app.css";
+import { HookCompoent } from "./HookComponent";
 
 const areaStyle: React.CSSProperties = {
   position: "absolute",
   right: "0px",
-  width: "200px"
+  width: "100%",
+  textAlign: "center"
 };
 
 const App: React.FC<{}> = () => {
   return (
     <div className={styles.AppStyle}>
       <h1 style={areaStyle}>HI!</h1>
+      <HookCompoent />
     </div>
   );
 };
